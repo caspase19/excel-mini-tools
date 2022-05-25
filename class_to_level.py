9b703c971658f6dabc3b8b27cd97131ac236ad8f
caspase19 <wangzb16@outlook.com>
2022-05-25 14:55:11 +0800

    add some notes

--- class_to_level.py
+++ class_to_level.py
@@ -1,5 +1,16 @@
+# 读取成绩单“ranks.xls”，给出某班分属两个给定分段的人数
+
 import xlrd
 
+# 生成某班学生成绩组成的列表
+def generate(class_num,subject_num):
+    list_1 = []
+    for i in range(nrows):
+        if ranks.cell_value(i,3) == class_num:
+            list_1.append(ranks.cell_value(i,subject_num))
+    return list_1
+
+# 统计列表中分属两个给定分段的人数
 def counts(list1, score1, score2):
     a, b = 0, 0
     for i in range(len(list1)):
@@ -8,14 +19,6 @@ def counts(list1, score1, score2):
         elif list1[i] <= score2:
             b += 1
     return a, b
-
-def generate(class_num,subject_num):
-    list_1 = []
-    for i in range(nrows):
-        if ranks.cell_value(i,3) == class_num:
-            list_1.append(ranks.cell_value(i,subject_num))
-    return list_1
-
 
 book = xlrd.open_workbook("ranks.xls")
 ranks = book.sheet_by_index(0)
