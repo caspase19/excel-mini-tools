4cb4f4358ee14226279c2227c91dceef26527b65
caspase19 <wangzb16@outlook.com>
2022-05-26 14:11:58 +0800

    update name to level module

--- names_to_level.py
+++ names_to_level.py
@@ -1,4 +1,15 @@
 import xlrd
+import xlwt
+
+subject_tuple = ("总分","语文","数学","英语","物理","化学","生物","政治","历史","地理")
+
+#def level(class_num):
+#    if class_num in [7,8,9,10,11]:
+#       return 6000, 11000
+#    elif class_num in [12,13,14,15]:
+#        return 11000, 19000
+#    elif class_num in [16,17]:
+#        return 19000, 21000
 
 def counts(list1, score1, score2):
     a, b = 0, 0
@@ -9,21 +20,33 @@ def counts(list1, score1, score2):
             b += 1
     return a, b
 
-def generate(name_list,subject_num):
+def generate(name_list, subject_name):
+#   adjust this fomula according to the score sheet
+    subject_num = 5 + 4 * subject_tuple.index(subject_name)
+    if ranks.cell_value(1,subject_num) != "联考排名":
+        input("请调整成绩单，使总分的分数位于第E列，按任意键退出")
+        exit()
     list_1 = []
     for i in range(nrows):
         if name_list.count(ranks.cell_value(i,0)) == 1:
             list_1.append(ranks.cell_value(i,subject_num))
+    list_1 = [i for i in list_1 if i != ""]
     return list_1
-
 
 book = xlrd.open_workbook("ranks.xls")
 ranks = book.sheet_by_index(0)
 book = xlrd.open_workbook("records.xls")
 records = book.sheet_by_index(0)
+results = xlwt.Workbook(encoding = 'utf-8')
+results_sheet = results.add_sheet("结果")
 nrows = ranks.nrows
-# 调整以下参数，并验证名单是否正确
-name_list = records.col_values(1, start_rowx=29, end_rowx=63)
-print(name_list)
-print(counts(generate(name_list,42),6000,11000))
+for i in range(1,records.ncols):
+    name_list = records.col_values(i, start_rowx=4, end_rowx=None)
+    name_list = [i for i in name_list if i != ""]
+    list_1 = generate(name_list, records.cell_value(1,i))
+    result = counts(list_1,records.cell_value(2,i),records.cell_value(3,i))
+    results_sheet.write(4,i,result[0])
+    results_sheet.write(5,i,result[1])
+results.save("基数.xls")
+
 
