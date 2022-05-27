155984b02c816002012dc97eb3e26a258b6e6d59
caspase19 <wangzb16@outlook.com>
2022-05-27 09:48:48 +0800

    add subject_name to subject_num function

--- names_to_level.py
+++ names_to_level.py
@@ -1,44 +1,46 @@
 import xlrd
 import xlwt
 
-subject_tuple = ("总分","语文","数学","英语","物理","化学","生物","政治","历史","地理")
+def subject_index(subject_name):
+    subject_tuple = ("总分","语文","数学","英语","物理","化学","生物","政治","历史","地理")
+    subject_num = 5 + 4 * subject_tuple.index(subject_name)
+    return subject_num
 
-def counts(list1, score1, score2):
+def generate(name_list, subject_num):
+    rank_list = []
+    for i in range(ranks.nrows):
+        if name_list.count(ranks.cell_value(i,0)) == 1:
+            rank_list.append(ranks.cell_value(i,subject_num))
+    rank_list = [i for i in rank_list if i != ""]
+    return rank_list
+
+def counts(rank_list, high_level, medium_level):
     a, b = 0, 0
-    for i in range(len(list1)):
-        if list1[i] <= score1:
+    for i in range(len(rank_list)):
+        if rank_list[i] <= high_level:
             a += 1
-        elif list1[i] <= score2:
+        elif rank_list[i] <= medium_level:
             b += 1
     return a, b
-
-def generate(name_list, subject_name):
-#   adjust this fomula according to the score sheet
-    subject_num = 5 + 4 * subject_tuple.index(subject_name)
-    if ranks.cell_value(1,subject_num) != "联考排名":
-        input("请调整成绩单，使总分的分数位于第E列，按任意键退出")
-        exit()
-    list_1 = []
-    for i in range(nrows):
-        if name_list.count(ranks.cell_value(i,0)) == 1:
-            list_1.append(ranks.cell_value(i,subject_num))
-    list_1 = [i for i in list_1 if i != ""]
-    return list_1
 
 book = xlrd.open_workbook("ranks.xls")
 ranks = book.sheet_by_index(0)
 book = xlrd.open_workbook("records.xls")
 records = book.sheet_by_index(0)
 results = xlwt.Workbook(encoding = 'utf-8')
-results_sheet = results.add_sheet("结果")
-nrows = ranks.nrows
+results_sheet = results.add_sheet("走班基数")
+for i in range(4):
+    for j in range(records.ncols):
+        results_sheet.write(i,j,records.cell_value(i,j))
+
 for i in range(1,records.ncols):
     name_list = records.col_values(i, start_rowx=4, end_rowx=None)
     name_list = [i for i in name_list if i != ""]
-    list_1 = generate(name_list, records.cell_value(1,i))
-    result = counts(list_1,records.cell_value(2,i),records.cell_value(3,i))
+    subject_num = subject_index(records.cell_value(1,i))
+    rank_list = generate(name_list,subject_num)
+    result = counts(rank_list,records.cell_value(2,i),records.cell_value(3,i))
     results_sheet.write(4,i,result[0])
     results_sheet.write(5,i,result[1])
-results.save("基数.xls")
+results.save("走班基数.xls")
 
 
